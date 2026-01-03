11578974c541a0f92f427abbd8324fd767da8105
mdikifahriza <147649626+mdikifahriza@users.noreply.github.com>
2026-01-03 11:44:26 +0700

    Delete src directory

--- src/hooks/index.ts
+++
@@ -1,9 +0,0 @@
-// src/hooks/index.ts
-// Re-export all hooks
-
-export * from './useTeachers';
-export * from './useClasses';
-export * from './useSubjects';
-export * from './useRooms';
-export * from './useGARunner';
-export * from './useNavigate';
