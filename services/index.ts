0d1498c27b00fef757678b68d78cd56e2e6192f8
mdikifahriza <147649626+mdikifahriza@users.noreply.github.com>
2026-01-03 11:44:07 +0700

    Delete services directory

--- services/index.ts
+++
@@ -1,8 +0,0 @@
-// src/services/index.ts
-// Re-export all services for easier imports
-
-export * from './teacherService';
-export * from './classService';
-export * from './subjectService';
-export * from './gaService';
-export * from './scheduleService';
